699a4a38edc1f5b61994e937633a6c26bb5d00af
Benaja <32866461+benaja@users.noreply.github.com>
2022-03-27 13:46:28 +0545

    Initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Benaja\StatamicWebsiteBuilder;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        // sdfdsadsfdssdfdsfafa
+    }
+}
